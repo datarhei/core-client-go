2a73b1cd6fc06cc6ae7d49764e0b8b97020aa7b0
Ingo Oppermann <ingo@datarhei.com>
2022-10-14 17:32:38 +0200

    Add general filesystem access, allow different config versions

--- diskfs.go
+++ diskfs.go
@@ -1,59 +1,27 @@
 package coreclient
 
 import (
-	"encoding/json"
 	"io"
-	"net/url"
 
 	"github.com/datarhei/core-client-go/v16/api"
 )
 
-const (
-	SORT_DEFAULT  = "none"
-	SORT_NONE     = "none"
-	SORT_NAME     = "name"
-	SORT_SIZE     = "size"
-	SORT_LASTMOD  = "lastmod"
-	ORDER_DEFAULT = "asc"
-	ORDER_ASC     = "asc"
-	ORDER_DESC    = "desc"
-)
-
 func (r *restclient) DiskFSList(sort, order string) ([]api.FileInfo, error) {
-	var files []api.FileInfo
-
-	values := url.Values{}
-	values.Set("sort", sort)
-	values.Set("order", order)
-
-	data, err := r.call("GET", "/v3/fs/disk?"+values.Encode(), "", nil)
-	if err != nil {
-		return files, err
-	}
-
-	err = json.Unmarshal(data, &files)
-
-	return files, err
+	return r.FilesystemList("disk", "", sort, order)
 }
 
 func (r *restclient) DiskFSHasFile(path string) bool {
-	_, err := r.call("HEAD", "/v3/fs/disk"+path, "", nil)
-
-	return err == nil
+	return r.FilesystemHasFile("disk", path)
 }
 
 func (r *restclient) DiskFSGetFile(path string) (io.ReadCloser, error) {
-	return r.stream("GET", "/v3/fs/disk"+path, "", nil)
+	return r.FilesystemGetFile("disk", path)
 }
 
 func (r *restclient) DiskFSDeleteFile(path string) error {
-	_, err := r.call("DELETE", "/v3/fs/disk"+path, "", nil)
-
-	return err
+	return r.FilesystemDeleteFile("disk", path)
 }
 
 func (r *restclient) DiskFSAddFile(path string, data io.Reader) error {
-	_, err := r.call("PUT", "/v3/fs/disk"+path, "application/data", data)
-
-	return err
+	return r.FilesystemAddFile("disk", path, data)
 }
